7446486c2fa4c9d875c2e3c25a528775b10b44d3
Priyanshu Soni <priyanshu171203@gmail.com>
2025-08-06 09:52:24 +0530

    distributed lock using kv clerk completed

--- src/lock/lock.go
+++ src/lock/lock.go
@@ -12,8 +12,9 @@ type Lock struct {
 	// the specific Clerk type of ck but promises that ck supports
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
-	ck  kvtest.IKVClerk
-	key string
+	ck       kvtest.IKVClerk
+	key      string
+	clientID string
 	// You may add code here
 }
 
@@ -23,30 +24,48 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck, key: l}
+	lk := &Lock{ck: ck, key: l, clientID: kvtest.RandValue(8)}
 	// You may add code here
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	for {
-		err := lk.ck.Put(lk.key, "locked", 0)
-		if err == rpc.OK {
-			log.Printf("Acquired lock")
-			return
+		value, version, err := lk.ck.Get(lk.key)
+		if err == rpc.ErrNoKey {
+			putErr := lk.ck.Put(lk.key, lk.clientID, 0)
+			if putErr == rpc.OK {
+				log.Printf("Lock acquired for key %s", lk.key)
+				return
+			}
 		} else {
-			time.Sleep(100 * time.Millisecond)
-			log.Printf("Sleeping for 100ms")
+			//log.Printf("Lock acquired for key: %v, value: %v, version: %v", lk.key, value, version)
+			if value == "unlocked" {
+				putErr := lk.ck.Put(lk.key, lk.clientID, version)
+				if putErr == rpc.OK {
+					log.Printf("Lock acquired for key which was existing %s", lk.key)
+					return
+				}
+			} else {
+				log.Printf("First unlock it for key %s", lk.key)
+				time.Sleep(1 * time.Second)
+			}
 		}
 	}
 }
 
 func (lk *Lock) Release() {
 	value, version, err := lk.ck.Get(lk.key)
-	log.Printf("Got lock value: %v, version: %v", value, version)
+	//log.Printf("error %v", err)
+	//log.Printf("Got lock value: %v, version: %v", value, version)
 	if err != rpc.OK {
 		log.Printf("Got lock error: %v", err)
-		return
+	} else if value == lk.clientID {
+		log.Printf("Release lock")
+		lk.ck.Put(lk.key, "unlocked", version)
+	} else {
+		log.Printf("Its not your lock to release it.")
 	}
+	return
 	//lk.ck.Put(lk.key, "", version)
 }
